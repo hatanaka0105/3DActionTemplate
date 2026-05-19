66a40494d0842e029670a7000d685ef1a2e0a259
CCCCradle <Daybreak060105@gmail.com>
2026-05-19 14:05:02 +0900

    Add FireBall class for projectile behavior

---
+++ 3DActionTemplate/Assets/FireBall.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _effect;
+    [SerializeField]
+    private float _power = 1000f;
+
+    private Vector3 _dir;
+    private bool _isShooting = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var colliders = Physics.OverlapSphere(transform.position, 5f);
+        foreach (var collider in colliders)
+        {
+            var rigidbody = collider.GetComponent<Rigidbody>();
+            if(rigidbody != null)
+            {
+                rigidbody.AddForce(transform.position - collider.transform.position * _power);
+            }
+        }
+        Instantiate(_effect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if(_isShooting)
+        {
+            transform.position += _dir * Time.deltaTime;
+        }
+    }
+
+    public void Shoot(Vector3 dir)
+    {
+        _dir = dir;
+        _isShooting = true;
+    }   
+}
